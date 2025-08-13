ef7f9dd6a12eab714ea8c3a242062ddd4c8e21cc
Ebenhaiser Jonathan Caprisiano <ebenhaiser.jonathan@corp.winning-soft.com>
2025-08-13 15:01:37 +0700

    adding listen to keyboard

--- assets/script.js
+++ assets/script.js
@@ -10,9 +10,45 @@ function backspace() {
   document.getElementById("display").value = currentDisplay;
 }
 
-function clearDisplay() {
-  currentDisplay = "";
-  document.getElementById("display").value = "";
+function handleKeyPress(event) {
+  const key = event.key;
+
+  const keyMap = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    "+": "+",
+    "-": "-",
+    "*": "*",
+    "/": "/",
+    ".": ".",
+    ",": ".", // Support comma as decimal separator
+    Enter: "=",
+    "=": "=",
+    Escape: "C",
+    Backspace: "backspace",
+  };
+
+  const mappedKey = keyMap[key];
+
+  if (mappedKey) {
+    if (mappedKey === "=") {
+      calculate();
+    } else if (mappedKey === "C") {
+      clearDisplay();
+    } else if (mappedKey === "backspace") {
+      backspace();
+    } else {
+      appendToDisplay(mappedKey);
+    }
+  }
 }
 
 function calculate() {
@@ -25,4 +61,9 @@ function calculate() {
     document.getElementById("display").value = "Error";
     currentDisplay = "";
   }
+}
+
+function clearDisplay() {
+  currentDisplay = "";
+  document.getElementById("display").value = "";
 }
